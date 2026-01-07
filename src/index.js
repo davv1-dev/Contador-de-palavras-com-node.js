c1658166dc79a0148e406bf9ac0d08f50f6bfe80
davv1-dev <daviamaral566@gmail.com>
2026-01-07 16:04:33 -0300

    logica de contagem pronta(porem nao tratada)

--- src/index.js
+++ src/index.js
@@ -2,8 +2,21 @@ const fs = require('fs');
 const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2]
 fs.readFile(link, 'utf-8', (erro,texto)=>{
-    verificaPalavrasDuplicadas(texto);
+    quebraEmparagrafos(texto);
+    //verificaPalavrasDuplicadas(texto);
 })
+function quebraEmparagrafos (texto){
+    const paragrafos = texto.toLowerCase().split('\n');
+    const contagem = paragrafos.map((paragrafo) =>{
+        return verificaPalavrasDuplicadas(paragrafo);
+    })
+    console.log(contagem)
+} 
 function verificaPalavrasDuplicadas(texto){
-
+const listapalavras = texto.split(' ');
+const resultado = {};
+listapalavras.forEach(palavra => {    
+    resultado[palavra] = (resultado[palavra] || 0)+1
+})
+return resultado;
 }
