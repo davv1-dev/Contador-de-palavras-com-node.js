9f13bdaa1da7fc37f9245eacc19e08746f9e067d
davv1-dev <daviamaral566@gmail.com>
2026-01-28 14:49:07 -0300

    adicionando chalk

--- src/cli.js
+++ src/cli.js
@@ -4,6 +4,7 @@ import trataErros from './erros/funcoesErro.js';
 import { contaPalavras } from './index.js';
 import { montaSaidaAquivo } from './helpers.js';
 import { Command } from 'commander';
+import  chalk  from 'chalk';
 
 const program = new Command();
 
@@ -15,7 +16,7 @@ program
         const {texto, destino } = options;
 
         if(!texto || !destino){
-            console.error('Erro por favor inserir caminho de origem e destino')
+            console.error(chalk.red('Erro por favor inserir caminho de origem e destino'));
             program.help();
             return;
         }
@@ -25,7 +26,7 @@ program
 
         try{
             processaAquivo(caminhoTexto,caminhoDestino);
-            console.log('Texto processado com sucesso');
+            console.log(chalk.green('Texto processado com sucesso'));
 
         }catch(erro){
             console.log('ocorreu um erro no processamento',erro);
